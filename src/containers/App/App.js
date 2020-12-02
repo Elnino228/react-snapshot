8a0e7480ed61efb21f9a57c1250b71418c74517e
elnino228 <daodat228@gmail.com>
2020-12-02 22:28:08 +0700

    add navigator

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Header from "../Header/Header";
 import SearchForm from "../SearchForm/SearchForm";
 import './App.scss'
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import Navigator from "../Navigator/Navigator";
 
 export default function App() {
     return (
         <div className={'app'}>
-            <Header title={'Snapshot'}/>
-            <SearchForm/>
+            <HashRouter>
+                <Header title={'Snapshot'}/>
+                <SearchForm/>
+                <Navigator/>
+            </HashRouter>
         </div>
     )
 }
