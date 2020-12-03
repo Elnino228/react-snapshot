612a8148432d684cd0fee66dcd53ef7039de83eb
Dao Van Dat (FIS FSB HN) <DatDV15@fpt.com.vn>
2020-12-03 17:14:53 +0700

    3

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 import Header from "../Header/Header";
 import SearchForm from "../SearchForm/SearchForm";
 import './App.scss'
-import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
 import Navigator from "../Navigator/Navigator";
 
 export default function App() {
     return (
         <div className={'app'}>
-            <HashRouter>
-                <Header title={'Snapshot'}/>
-                <SearchForm/>
-                <Navigator/>
-            </HashRouter>
+            <BrowserRouter basename={'SnapShot1'}>
+                <Route render={props => (
+                    <>
+                        <Header title={'Snapshot'}/>
+                        <SearchForm/>
+                        <Navigator/>
+                    </>
+                )}/>
+            </BrowserRouter>
         </div>
     )
 }
